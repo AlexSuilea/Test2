37141b776a5571d14a6b0ff84e754669d778e787
AlexSuilea <42109259+AlexSuilea@users.noreply.github.com>
2019-06-01 01:07:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -79,17 +79,37 @@ $.getJSON('https://www.mocky.io/v2/58fda6ce0f0000c40908b8c8', function(data) {
     $(".detail14").append(detail14);
 });
 
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1;}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 15000);
+  dots[slideIndex-1].className += " active";
 }
+
+var timer=null;
+function setTimer(){
+  timer=setInterval(function(){
+    plusSlides(1);
+  },15000);
+}
+setTimer();
